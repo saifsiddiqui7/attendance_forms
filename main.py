181979bbf99064f03286aeccf5ad9443748bbaf6
Saif Siddiqui <apple@Saifs-MBP.attlocal.net>
2021-01-20 19:34:38 -0500

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import requests
 
-r = requests.get('https://docs.google.com/forms/d/e/1FAIpQLSexwRVI3f4daEVMIxYzVAG2UMXDsVAW6AbsMMekKgF7MvuWrA/viewform')
-
-print(r.text)
+url = 'https://docs.google.com/forms/d/e/1FAIpQLSexwRVI3f4daEVMIxYzVAG2UMXDsVAW6AbsMMekKgF7MvuWrA/formResponse'
+values = {
+    # Last Name
+    'entry.616020434': 'Siddiqui',
+    # First Name
+    'entry.570950898': 'Saif',
+    # Color Group
+    'entry.2110622356': 'Monday, Tuesday, Thursday (I have a purple box on my ID)',
+    # Class Period
+    'entry.816737777': '1 AP Physics C Mechanics',
+}
